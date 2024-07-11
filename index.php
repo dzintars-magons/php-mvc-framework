3e0bbc2615280bef0013f927dd22ddbf38d66870
Dzintars <dzintars.magons@gmail.com>
2024-07-11 08:43:56 +0300

    autoloader on classes with namespaces

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 // require "src/router.php";
 
 spl_autoload_register(function (string $class_name) {
-    require "src/$class_name.php";
+    require "src/" . str_replace("\\", "/", $class_name) . ".php";
 });
 
-$router = new Router;
+$router = new Framework\Router;
 
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
@@ -23,9 +23,7 @@ if ($params === false) {
 }
 
 $action = $params['action'];
-$controller = $params['controller'];
-
-require "src/controllers/$controller.php";
+$controller = "App\Controllers\\" . ucwords($params['controller']);
 
 $controller_object = new $controller;
 
