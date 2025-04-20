05a74665d7f1c477417d29d4d31a5dfdc9165302
Dzintars <dzintars.magons@gmail.com>
2025-04-20 21:38:38 +0300

    patterns using regex

--- index.php
+++ index.php
@@ -14,9 +14,10 @@ $router = new Framework\Router;
 
 //let's add a route for the home index path
 
-$router->add("/home/index", ["controller" => "home", "action" => "index"]);
-$router->add("/products", ["controller" => "products", "action" => "index"]);
-$router->add("/", ["controller" => "home", "action" => "index"]);
+$router->add("/{controller}/{action}");
+// $router->add("/home/index", ["controller" => "home", "action" => "index"]);
+// $router->add("/products", ["controller" => "products", "action" => "index"]);
+// $router->add("/", ["controller" => "home", "action" => "index"]);
 
 $params = $router->match($path);
 
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -10,7 +10,7 @@ class Router
     // We add a public method to add a route
     // The arguments are the route path, which is a string, and an array of parameters
     //This method isn't going to return anything so we'll specify the return type declaration as void
-    public function add(string $path, array $params): void
+    public function add(string $path, array $params = []): void
     {
         // To add the route to the routing table we'll append an element to the routes property
         // Each element will be an array , containing the path and its parameters
@@ -22,12 +22,31 @@ class Router
 
     public function match(string $path): array|bool
     {
+
         foreach ($this->routes as $route) {
-            if ($route["path"] === $path) {
-                return $route["params"];
+            $pattern = '#^/(?<controller>[a-z]+)/(?<action>[a-z]+)$#';
+
+            echo $pattern, "\n", $route["path"], "\n";
+
+            $this->getPatternFromRoutePath($route['path']);
+
+            if (preg_match($pattern, $path, $matches)) {
+                $matches = array_filter($matches, "is_string", ARRAY_FILTER_USE_KEY);
+            return $matches;
             }
         }
 
         return false;
     }
+
+    
+    private function getPatternFromRoutePath(string $route_path)
+    {
+        $route_path = trim($route_path, "/");
+        $segments = explode("/", $route_path);
+        $segments = array_map(function(string $segment): string {
+            return $segment;
+        }, $segments);
+        print_r($segments);
+    } 
 }
