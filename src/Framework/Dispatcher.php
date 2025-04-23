390d4cd9e22de7da66b12261e6b12f85aa669d62
Dzintars <dzintars.magons@gmail.com>
2025-04-23 20:28:08 +0300

    Dispatcher update

--- src/Framework/Dispatcher.php
+++ src/Framework/Dispatcher.php
@@ -59,6 +59,10 @@ class Dispatcher
 
         $namespace = "App\Controllers";
 
+        if (array_key_exists("namespace", $params)) {
+            $namespace .= "\\" . $params["namespace"];
+        }
+
         return $namespace . "\\" . $controller;
     }
 
