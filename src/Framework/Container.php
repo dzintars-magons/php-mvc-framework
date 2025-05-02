27790a3293e77fa8f1f2e17b86d4e08aead3fccc
Dzintars <dzintars.magons@gmail.com>
2025-05-02 22:16:17 +0300

    InvalidArgumentException to get more details

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -7,7 +7,7 @@ namespace Framework;
 use ReflectionClass;
 use Closure;
 use ReflectionNamedType;
-use Exception;
+use InvalidArgumentException;
 
 class Container
 {
@@ -32,17 +32,17 @@ class Container
         foreach ($constructor->getParameters() as $parameter) {
             $type = $parameter->getType();
             if ($type === null) {
-                throw new Exception("Constructor parameter '{$parameter->getName()}'
+                throw new InvalidArgumentException("Constructor parameter '{$parameter->getName()}'
                 in the $class_name class
                 has no type declaration");
             }
             if (! ($type instanceof ReflectionNamedType)) {
-                exit("Constructor parameter '{$parameter->getName()}'
+                throw new InvalidArgumentException("Constructor parameter '{$parameter->getName()}'
                 in the $class_name class is an invalid type: '$type'
                 - only single named types supported");
             }
             if ($type->isBuiltin()) {
-                exit("Unable to resolve constructor parameter
+                throw new InvalidArgumentException("Unable to resolve constructor parameter
                     '{$parameter->getName()}'
                     of type '$type' in the $class_name class");
             }
