185d69669789370071e160c43baf19302f485858
Dzintars <dzintars.magons@gmail.com>
2025-05-02 21:59:12 +0300

    Validate the Type Declarations in the Service Container

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,6 +4,7 @@ namespace Framework;
 
 use ReflectionClass;
 use Closure;
+use ReflectionNamedType;
 
 class Container
 {
@@ -27,6 +28,16 @@ class Container
         }
         foreach ($constructor->getParameters() as $parameter) {
             $type = $parameter->getType();
+            if ($type === null) {
+                exit("Constructor parameter '{$parameter->getName()}'
+                in the $class_name class
+                has no type declaration");
+            }
+            if (! ($type instanceof ReflectionNamedType)) {
+                exit("Constructor parameter '{$parameter->getName()}'
+                in the $class_name class is an invalid type: '$type'
+                - only single named types supported");
+            }
             if ($type->isBuiltin()) {
                 exit("Unable to resolve constructor parameter
                     '{$parameter->getName()}'
